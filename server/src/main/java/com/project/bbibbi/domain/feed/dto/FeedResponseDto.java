2a400b7ef25b80acc712b6fcbb848a10dec7397d
seonhoshin <shinwder@gmail.com>
2023-08-31 21:20:59 +0900

    ccc

---
+++ server/src/main/java/com/project/bbibbi/domain/feed/dto/FeedResponseDto.java
@@ -0,0 +1,59 @@
+package com.project.bbibbi.domain.feed.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeedResponseDto {
+    private Long feedId;
+
+    private LocalDateTime createdDateTime;
+
+    private LocalDateTime modifiedDateTime;
+
+    private String title;
+
+    private String content;
+
+    private int views;
+
+    private String coverPhoto;
+
+    // enum 생기면 아래 내용처럼 처리
+    //private RoomType roomType;
+
+    private String roomType;
+    private String roomTypeName;
+    private String roomSize;
+    private String roomSizeName;
+    private String roomCount;
+    private String roomCountName;
+    private String roomInfo;
+    private String roomInfoName;
+    private String location;
+    private String locationName;
+
+//    public String getRoomType() {
+//        return roomType.getType();
+//    }
+
+    private Long memberId;
+    private String nickname;
+
+    // feedImage와 합쳐보내
+    private List<FeedImageDto> feedImages;
+
+    // 이 게시물의 좋아요 개수
+    private Integer likeCount;
+
+
+    // 답변내용을 여기에다가 넣을건지? 아니면 따로 답변요청을 할건지?
+    // 답변 무한스크롤 하려면 여기 넣기 어려움.
+
+}
---
+++ server/src/main/java/com/project/bbibbi/domain/feed/entity/FeedImage.java
@@ -0,0 +1,38 @@
+package com.project.bbibbi.domain.feed.entity;
+
+import com.project.bbibbi.global.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class FeedImage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long feedImageId;
+
+    @Column(length = 3000)
+    private String image;
+
+    @Column
+    private Double x;
+
+    @Column
+    private Double y;
+
+    @Column(length = 300)
+    private String tag;
+
+    @ManyToOne
+    @JoinColumn(name = "feed_id")
+    private Feed feed;
+
+}
